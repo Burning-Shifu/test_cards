40e43dc45da1441948fcbe57942678b9720330a4
Shifu <kris.deniso@mail.ru>
2021-03-27 17:48:27 +0300

    added filtration by ID

--- app/js/main.js
+++ app/js/main.js
@@ -1,16 +1,16 @@
 document.addEventListener('DOMContentLoaded', () => {
 
-  const postData = async (url, data) => {  
-    let res = await fetch(url, {
-      method: 'POST',
-      headers: {
-        'Content-type': 'application/json'
-      },
-      body: data
-    });
+  // const postData = async (url, data) => {  
+  //   let res = await fetch(url, {
+  //     method: 'POST',
+  //     headers: {
+  //       'Content-type': 'application/json'
+  //     },
+  //     body: data
+  //   });
   
-    return await res.json(); 
-  };
+  //   return await res.json(); 
+  // };
 
   
   async function getResource(url) {  
@@ -23,14 +23,16 @@ document.addEventListener('DOMContentLoaded', () => {
     return await res.json();
   }
   
+  // вывод карточек из бд
 
   function cards() {
     class OrderCard {
-      constructor(id, createDate, order, type, ...classes) {
+      constructor(id, createDate, order, type, time, ...classes) {
         this.id = id;
         this.createDate = createDate;
         this.order = order;
         this.type = type;
+        this.time = time;
         this.classes = classes;  // важно помнить, что это массив, даже если он пустой
         this.parent = document.querySelector(".cards");
       }
@@ -55,7 +57,9 @@ document.addEventListener('DOMContentLoaded', () => {
           <div class="cards__content">
             <p class="cards__id">${this.id}</p>
             <p class="cards__create-date">${this.createDate}</p>
-          </div>
+            <p class="cards__create-date">${this.time}</p>
+            <p class="cards__create-date">${this.type}</p>
+            </div>
         `;
 
         this.parent.append(element);
@@ -64,12 +68,85 @@ document.addEventListener('DOMContentLoaded', () => {
 
     getResource('http://localhost:3000/cards')
         .then(data => {
-          data.forEach(({id, createDate, order, type}) => {    // деструктуризируем объект
-            new OrderCard(id, createDate, order, type).render();
+          data.forEach(({id, createDate, order, type, time}) => {    // деструктуризируем объект
+            new OrderCard(id, createDate, order, type, time).render();
           });
         });
   }
 
   cards();
+
+  // end вывод карточек из бд
+
+  // модальные окна
+  const btnModal = document.querySelector('.sort__add'),
+    btnClose = document.querySelector('.modal__close'),
+    addModal = document.querySelector('.modal'),
+    body = document.querySelector('body'),
+    form = document.querySelector('.book-modal__form');
+
+  let scrollWidth = window.innerWidth - document.documentElement.clientWidth;
+      
+
+  function openModal(modal) {
+    modal.classList.remove('hide');
+    modal.classList.add('show');
+    body.style.overflow = 'hidden';
+    body.style.paddingRight = scrollWidth + 'px';
+  }
+
+  btnModal.addEventListener('click', () => openModal(addModal));
+
+  function closeModal(modal) {
+    modal.classList.remove('show');
+    modal.classList.add('hide');
+    body.style.overflow = 'auto';
+    body.style.paddingRight = 0 + 'px';
+  }
+
+  btnClose.addEventListener('click', () => closeModal(addModal));
+
+  document.addEventListener('keydown', (e) => {
+    if (e.key === 'Escape' && addModal.classList.contains('show')) {
+      closeModal(addModal);
+    }
+  });
+
+  // submit
+
+  // form.addEventListener('submit', (e) => {
+  //   e.preventDefault();
+
+  //   form.reset();
+  //   closeModal(modalBook);
+  //   openModal(modalThx);
+  // });
+
+  // end модальные окна
+
+
+  const cardsList = document.querySelectorAll(".cards__item");
+
+  const cardsId = document.querySelectorAll(".cards__id"),
+        inputId = document.querySelector("#filter-id");
+
+  function filterById(param) {
+    
+    cardsId.forEach((item) => {
+      console.log('ok');
+      if (item.textContent !== param && param !== "") {
+        item.parentElement.parentElement.style.display = "none";
+      } else {
+        item.parentElement.parentElement.style.display = "";
+      }
+    });
+    console.log(param);
+  }
+
+  inputId.addEventListener('input', () => filterById(inputId.value));
+
+  
+
+  
 
 });
